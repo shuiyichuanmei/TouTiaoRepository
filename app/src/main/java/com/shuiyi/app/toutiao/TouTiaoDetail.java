b40e0bdd57962756978c9ecc99707cab037b45d0
sheshou89 <373139304@qq.com>
2016-06-19 10:29:12 +0800

    wpc

--- app/src/main/java/com/shuiyi/app/toutiao/TouTiaoDetail.java
+++ app/src/main/java/com/shuiyi/app/toutiao/TouTiaoDetail.java
@@ -23,10 +23,8 @@ import java.util.ArrayList;
 public class TouTiaoDetail extends AppCompatActivity {
     private ProgressBar progressBar;
     private String news_url;
-    private String news_title;
-    private String news_source;
-    private String news_date;
     WebView webView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,8 +32,7 @@ public class TouTiaoDetail extends AppCompatActivity {
         //setNeedBackGesture(true);//设置需要手势监听
 
         Intent intent1 = getIntent();
-        news_url="http://toutiao.ishowyou.cc/appdetail.aspx?id="+intent1.getStringExtra("id");
-        //getData();
+        news_url = "http://toutiao.ishowyou.cc/appdetail.aspx?id=" + intent1.getStringExtra("id");
         initView();
         initWebView();
     }
@@ -44,22 +41,23 @@ public class TouTiaoDetail extends AppCompatActivity {
         progressBar = (ProgressBar) findViewById(R.id.ss_htmlprogessbar);
         progressBar.setVisibility(View.VISIBLE);
     }
+
     private void initWebView() {
-        webView = (WebView)findViewById(R.id.wb_details);
+        webView = (WebView) findViewById(R.id.wb_details);
         if (!TextUtils.isEmpty(news_url)) {
             WebSettings settings = webView.getSettings();
             settings.setJavaScriptEnabled(true);//设置可以运行JS脚本
-			//settings.setTextZoom(100);//设置页面的文本缩放百分比。默认值是100。
+            //settings.setTextZoom(100);//设置页面的文本缩放百分比。默认值是100。
             settings.setLayoutAlgorithm(LayoutAlgorithm.SINGLE_COLUMN);
-			settings.setUseWideViewPort(true); //打开页面时， 自适应屏幕??
-			settings.setLoadWithOverviewMode(true);//缩小内容以适合屏幕宽度
+            settings.setUseWideViewPort(true); //打开页面时， 自适应屏幕??
+            settings.setLoadWithOverviewMode(true);//缩小内容以适合屏幕宽度
             settings.setSupportZoom(false);// 用于设置webview放大
             settings.setBuiltInZoomControls(false);
             webView.setBackgroundResource(R.color.transparent);
             //webView.addJavascriptInterface(new JavascriptInterface(getApplicationContext()),"imagelistner");// 添加js交互接口类，并起别名 imagelistner
             webView.setWebChromeClient(new MyWebChromeClient());//设置加载进度
             webView.setWebViewClient(new MyWebViewClient());
-            new MyAsnycTask().execute(news_url, news_title, news_source + " " +news_date);
+            new MyAsnycTask().execute(news_url);
         }
     }
 
@@ -69,12 +67,11 @@ public class TouTiaoDetail extends AppCompatActivity {
 //        overridePendingTransition(R.anim.slide_in_left, R.anim.slide_out_right);
 //    }
 
-    private class MyAsnycTask extends AsyncTask<String, String,String>{
+    private class MyAsnycTask extends AsyncTask<String, String, String> {
 
         @Override
         protected String doInBackground(String... urls) {
-            String data="<div id=\"divneirong\" class=\"tuwen_content\">\n" +
-                    "                    <p><br></p><p><img src=\"http://p3.pstatp.com/large/7210005eed0ff6b7752\" img_width=\"461\" img_height=\"303\" alt=\"比亚迪的一个数字堵住所有喷国产车的人的嘴\" onerror=\"javascript:errorimg.call(this);\"></p><p><img src=\"http://p3.pstatp.com/large/7210005eedbc878a6bf\" img_width=\"611\" img_height=\"408\" alt=\"比亚迪的一个数字堵住所有喷国产车的人的嘴\" onerror=\"javascript:errorimg.call(this);\"></p><p>“542”，对，就是这么一个简单的阿拉伯数字，可以堵住所有喷国产车的人的嘴，因为这个数字背后凝聚了一个巨大的变革。是中国汽车工业弯道超车的机遇，打破了传统的燃油发动机技术，以电池电力化解能源危机。</p><p><img src=\"http://p3.pstatp.com/large/778000311e3bd397c38\" img_width=\"520\" img_height=\"359\" alt=\"比亚迪的一个数字堵住所有喷国产车的人的嘴\" onerror=\"javascript:errorimg.call(this);\"></p><p>5：代表比亚迪车型百公里加速在5秒以内，打破豪车专属；</p><p>4：代表全时电四驱，比传统的燃油四驱更有劲，更安全；</p><p>2：百公里综合油耗2L以内，打破SUV“油老虎”的弊端。</p><p><img src=\"http://p3.pstatp.com/large/7780003133641cfd1a1\" img_width=\"728\" img_height=\"456\" alt=\"比亚迪的一个数字堵住所有喷国产车的人的嘴\" onerror=\"javascript:errorimg.call(this);\"></p><p><strong>世界上，还有第二个汽车品牌能做到这个的性能指数吗？</strong>所以，不要再喷国产车了，现在已经不是那个看外国人脸色的年代了。</p><p>之前，总是有人笑话吐槽比亚迪插电混动车型，现在再回过头来看看，哪个品牌没有在研发这样的车型？就连一向固执坚持做普锐斯的丰田也开始了插电混动的研发，你们还有什么资格吐槽国产车落后？</p><p><img src=\"http://p3.pstatp.com/large/778000311eafcab1ddb\" img_width=\"500\" img_height=\"280\" alt=\"比亚迪的一个数字堵住所有喷国产车的人的嘴\" onerror=\"javascript:errorimg.call(this);\"></p><p><img src=\"http://p3.pstatp.com/large/778000313032add097b\" img_width=\"597\" img_height=\"477\" alt=\"比亚迪的一个数字堵住所有喷国产车的人的嘴\" onerror=\"javascript:errorimg.call(this);\"></p><p><img src=\"http://p3.pstatp.com/large/778000311ee0a519628\" img_width=\"500\" img_height=\"277\" alt=\"比亚迪的一个数字堵住所有喷国产车的人的嘴\" onerror=\"javascript:errorimg.call(this);\"></p><p><img src=\"http://p3.pstatp.com/large/778000311ef4071082a\" img_width=\"500\" img_height=\"265\" alt=\"比亚迪的一个数字堵住所有喷国产车的人的嘴\" onerror=\"javascript:errorimg.call(this);\"></p><p><img src=\"http://p2.pstatp.com/large/7210005ee1c7c46cec2\" img_width=\"500\" img_height=\"280\" alt=\"比亚迪的一个数字堵住所有喷国产车的人的嘴\" onerror=\"javascript:errorimg.call(this);\"></p><p><img src=\"http://p3.pstatp.com/large/7780003128733c24624\" img_width=\"882\" img_height=\"445\" alt=\"比亚迪的一个数字堵住所有喷国产车的人的嘴\" onerror=\"javascript:errorimg.call(this);\"></p><p>双模混动，两种行驶模式（到现在还有好多人都不了解什么是双模），无论是纯电动还是油电混动，动力都巨大！还有人担心纯电动模式下的动力不行，其实，这是一种错误的认识，大功率高转速的电机带来的动力，绝对不低于燃油车发动机的动力！上图的动力参数就可以看出！</p><p><img src=\"http://p1.pstatp.com/large/7210005eea4fd4e4c65\" img_width=\"536\" img_height=\"266\" alt=\"比亚迪的一个数字堵住所有喷国产车的人的嘴\" onerror=\"javascript:errorimg.call(this);\"></p><p>“542”，是一个新时代的开始，同时也是一个旧时代的结束！</p><p>不管你们看不看好，比亚迪，他都在引领一个新技术的变革！</p><p><br></p></div>";//NewsDetailsService.getNewsDetails(urls[0],urls[1],urls[2]);
+            String data = "";
             return data;
         }
 
@@ -107,7 +104,7 @@ public class TouTiaoDetail extends AppCompatActivity {
         }
 
         public void openImage(String img) {
-           System.out.println(img);
+            System.out.println(img);
             //
             String[] imgs = img.split(",");
             ArrayList<String> imgsUrl = new ArrayList<String>();
@@ -157,7 +154,7 @@ public class TouTiaoDetail extends AppCompatActivity {
     private class MyWebChromeClient extends WebChromeClient {
         @Override
         public void onProgressChanged(WebView view, int newProgress) {
-            if(newProgress != 100){
+            if (newProgress != 100) {
                 progressBar.setProgress(newProgress);
             }
             super.onProgressChanged(view, newProgress);
