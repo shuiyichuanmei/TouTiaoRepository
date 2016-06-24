b4719b5152e699f8cbdc3b9d04f51439c06faa97
Hey666 <200841345@qq.com>
2016-06-24 11:33:30 +0800

    ygutfguf

--- app/src/main/java/com/shuiyi/app/toutiao/PingTaiFragment.java
+++ app/src/main/java/com/shuiyi/app/toutiao/PingTaiFragment.java
@@ -1,22 +1,67 @@
 package com.shuiyi.app.toutiao;
 
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+
+import java.util.zip.Inflater;
+
+import static android.app.PendingIntent.getActivity;
 
 /**
  * Created by wang on 2016/6/7.
  */
 public class PingTaiFragment extends Fragment {
+    private LinearLayout DongFang;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.pingtai, container, false);
+        return inflater.inflate(R.layout.kuaican, container, false);
+
+    }
+
+    @Override
+    public void onHiddenChanged(boolean hidden) {
+        super.onHiddenChanged(hidden);
+
     }
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        findView();
     }
+    private void findView() {
+        DongFang=(LinearLayout) getActivity().findViewById(R.id.dongfang);
+        DongFang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), PingTaiDetailActivity.class);
+//                startActivity(intent);
+            }
+        });
+    }
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.pingtai);
+//        DongFang = (LinearLayout) this.findViewById(R.id.dongfang);
+//        DongFang.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent intent = new Intent(getActivity(), PingTaiDetailActivity.class);
+//                startActivity(intent);
+//            }
+//        });
+//
+//    }
+
+
 }
