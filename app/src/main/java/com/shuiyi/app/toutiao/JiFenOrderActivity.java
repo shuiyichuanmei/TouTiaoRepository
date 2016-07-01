f3bd8c6f9f39829d9ca7000ac6a7d88c06bf1f26
sheshou89 <373139304@qq.com>
2016-07-01 22:21:53 +0800

    home

--- app/src/main/java/com/shuiyi/app/toutiao/JiFenOrderActivity.java
+++ app/src/main/java/com/shuiyi/app/toutiao/JiFenOrderActivity.java
@@ -2,29 +2,44 @@ package com.shuiyi.app.toutiao;
 
 import android.app.Activity;
 import android.app.Fragment;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.Image;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.AdapterView;
+import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.PopupWindow;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import com.google.zxing.qrcode.encoder.QRCode;
 import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 import com.shuiyi.app.toutiao.adapter.JiFenOrderAdapter;
 import com.shuiyi.app.toutiao.adapter.KuaiCanAdapter;
 import com.shuiyi.app.toutiao.bean.JiFenOrderBean;
 import com.shuiyi.app.toutiao.bean.KuaiCanBean;
+import com.shuiyi.app.toutiao.bean.ShangChengBean;
 import com.shuiyi.app.toutiao.common.Common;
+import com.shuiyi.app.toutiao.common.QRCodeUtil;
 import com.shuiyi.app.toutiao.net.AsyncHttpUtil;
 import com.shuiyi.app.toutiao.view.CustomListView;
 
@@ -32,6 +47,7 @@ import org.apache.http.Header;
 import org.json.JSONArray;
 import org.json.JSONObject;
 
+import java.io.File;
 import java.util.ArrayList;
 
 /**
@@ -67,7 +83,7 @@ public class JiFenOrderActivity extends AppCompatActivity {
         listView = (CustomListView) findViewById(R.id.list_item);
         jifenList = new ArrayList<JiFenOrderBean>();
         listAdapter = new JiFenOrderAdapter(JiFenOrderActivity.this, jifenList);
-        imageButton =(ImageButton)this.findViewById(R.id.imageButton);
+        imageButton = (ImageButton) this.findViewById(R.id.imageButton);
         imageButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -91,6 +107,7 @@ public class JiFenOrderActivity extends AppCompatActivity {
                     listView.setVisibility(View.GONE);
                 }
             }
+
             @Override
             public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
                 Toast.makeText(JiFenOrderActivity.this, "网络异常", Toast.LENGTH_LONG).show();
@@ -114,6 +131,7 @@ public class JiFenOrderActivity extends AppCompatActivity {
                         jifenList.addAll(itemList);
                         listView.onRefreshComplete();
                     }
+
                     @Override
                     public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
                         Toast.makeText(JiFenOrderActivity.this, "网络异常", Toast.LENGTH_LONG).show();
@@ -138,12 +156,34 @@ public class JiFenOrderActivity extends AppCompatActivity {
                         jifenList.addAll(itemList);
                         listView.onLoadMoreComplete();
                     }
+
                     @Override
                     public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
                         Toast.makeText(JiFenOrderActivity.this, "网络异常", Toast.LENGTH_LONG).show();
                     }
                 };
                 GetHttpData(kc);
+            }
+        });
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                JiFenOrderBean item = (JiFenOrderBean) parent.getItemAtPosition(position);
+                String url = "http://toutiao.ishowyou.cc/jifen/houtai/OrderErweimaYanzheng.aspx?oid=" + item.getId();
+                Bitmap bitmap = QRCodeUtil.createQRImage(url, 300, 300);
+                View root = JiFenOrderActivity.this.getLayoutInflater().inflate(R.layout.ordererweima, null);
+                ImageView imgerweima = (ImageView) root.findViewById(R.id.imgerweima);
+                imgerweima.setImageBitmap(bitmap);
+
+                new AlertDialog.Builder(JiFenOrderActivity.this)
+                        .setView(root)
+                        .setTitle(item.getMingcheng())
+                        .setNegativeButton("关闭", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        }).create().show();
             }
         });
     }
